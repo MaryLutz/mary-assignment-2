b63c635d6486564e6f677fb514d95dcff7b08bf8
MaryLutz <151245619+MaryLutz@users.noreply.github.com>
2024-03-02 16:00:38 -0800

    Finished

--- src/com/coderscampus/Assignment2.java
+++ src/com/coderscampus/Assignment2.java
@@ -1,10 +1,45 @@
 package com.coderscampus;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Assignment2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Random random = new Random();
+		int randomNumber = random.nextInt(101);
+		System.out.println("Random number is " + randomNumber);
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Pick a number between 1 nad 100.");
 
+		int i = 1;
+		while (i <= 5){
+		int guessNumber = scanner.nextInt();
+				
+		if (guessNumber == randomNumber) {
+			System.out.println("You win!");
+			return;
+		}
+		 
+		else if (guessNumber < randomNumber){
+			System.out.println("Please pick a higher number.");	
+			 i++;
+		} 
+		
+		else if(guessNumber > randomNumber){
+			System.out.println("Please pick a lower number.");
+			 i++;
+		}
+		
 	}
-
+		System.out.println("You lose!");
+		
+	}
 }
+		
+			
+		
+
+
+
