3a3ef30561637620c2159f71139fd8e07406315d
MaryLutz <151245619+MaryLutz@users.noreply.github.com>
2024-03-08 17:50:37 -0800

    Last update
    Fix error and added less than 0 to account for negative numbers.

--- src/com/coderscampus/Assignment2.java
+++ src/com/coderscampus/Assignment2.java
@@ -12,17 +12,18 @@ public class Assignment2 {
 		System.out.println("Random number is " + randomNumber);
 
 		System.out.println("Pick a number between 1 nad 100.");
-
+		int a = 100;
+		int b = 0;
 		int i = 1;
 		while (i <= 5) {
 
 			Scanner scanner = new Scanner(System.in);
 			int guessNumber = scanner.nextInt();
-			if (guessNumber > 100 || guessNumber == 0) {
+			if (guessNumber > a || guessNumber <= b) {
 				System.out.println("Pick a number between 1 and 100.");
 			} else if (guessNumber == randomNumber) {
 				System.out.println("You win!");
-				scanner.close();
+				return;
 			} else if (guessNumber < randomNumber) {
 				System.out.println("Please pick a higher number.");
 				i++;
@@ -30,9 +31,8 @@ public class Assignment2 {
 				System.out.println("Please pick a lower number.");
 				i++;
 			}
-
 		}
 		System.out.println("You lose!");
-
+		
 	}
 }
